1ccad27ab8e12f79db81e18bddd21bd322c987c5
HoangTienManh <tienmanh541990@gmail.com>
2023-06-07 19:24:55 +0700

    update Pat's website

--- app.js
+++ app.js
@@ -1,33 +1,100 @@
 
+//  ==== functional function ====
+
+function getValueArrayObject(arr) {
+    let values = []
+    arr.forEach( item => {
+        values.push(item.sale)
+    })
+    return values
+}
+
+function getMin(arr) {
+    let min
+    min = Math.min(...arr)
+    return min
+}
+
+function getMax(arr) {
+    let max
+    max = Math.max(...arr)
+    return max
+}
+
+function getSumArray(arr) {
+    let sum = 0
+    arr.forEach( item => {
+        sum += item
+    })
+    return sum
+}
+
+function getAverage(arr) {
+    let avg = 0
+    let saleTimes = arr.length
+    let values = getValueArrayObject(arr)
+    let sum = getSumArray(values)
+    avg = Math.round(sum / saleTimes)
+
+    return avg
+}
+
+// ============= end functional function =============
+
+// ======= OOP ========
 
 function SaleData(hour) {
     this.hour = hour
-    this.sale = Math.round(Math.random() * 100)
+    this.sale = Math.floor(Math.random() * 100)
 }
 
-const saleDatas = [
-    new SaleData(6),
-    new SaleData(7),
-    new SaleData(8),
-    new SaleData(9),
-    new SaleData(10),
-    new SaleData(11),
-]
+function generateSaleDatas() {
+    let saleDatas = []
 
-const minmax = []
+    for (let i = 6; i <= 19; i++) {
+        let item = new SaleData(i)
+        saleDatas.push(item)
+    }
 
-saleDatas.forEach(item => {
-    keys = Object.keys(item)
-    keys.forEach(key => {
-       minmax.push(item[key])
-    })
-})
+    return saleDatas
+}
 
-console.log(minmax);
 
-function Location(name, min, max, saleData) {
+function Location(name, saleData) {
     this.name = name
-    this.min = min
-    this.max = max
-    this.saleData = saleData
+    this.min = getMin(getValueArrayObject(saleData))
+    this.max = getMax(getValueArrayObject(saleData))
+    this.avgSale = getAverage(saleData)
 }
+
+let seattle = new Location("Seattle", generateSaleDatas())
+let tokyo = new Location("Tokyo", generateSaleDatas())
+let dubai = new Location("Dubai", generateSaleDatas())
+let paris = new Location("Paris", generateSaleDatas())
+let lima = new Location("Lima", generateSaleDatas())
+
+// =======end OOP ========
+
+function render(location) {
+
+    let table = document.querySelector("#sale-table")
+
+    let tr = document.createElement("tr")
+    tr.innerHTML =
+                `<td>${location.name}</td>
+                <td>${location.min}</td>
+                <td>${location.max}</td>
+                <td>${location.avgSale}</td>`
+
+    table.appendChild(tr)
+}
+
+render(seattle)
+render(tokyo)
+render(dubai)
+render(paris)
+render(lima)
+
+
+
+
---
+++ draft.js
@@ -0,0 +1,3 @@
+let arr = [11,22,21,45,32]
+console.log(Math.min(...arr));
+console.log(Math.max(...arr));
